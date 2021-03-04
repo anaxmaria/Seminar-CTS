32548971023ed0e8698e85e0240c8b6583fd8f13
anaxmaria <63149698+anaxmaria@users.noreply.github.com>
2021-03-04 22:20:33 +0200

    New branch (#4)
    * pushed from intellij and made it work #2
    
    * pushed from intellij and made it work #3

--- src/ro/ase/cts/clase/readers/IReader.java
+++ src/ro/ase/cts/clase/readers/IReader.java
@@ -6,7 +6,9 @@ import java.util.List;
 import ro.ase.cts.clase.Aplicant;
 
 public abstract class IReader {
-	//initial fusese interfata
+
+	//initial fusese interfata, acum este clasa abstracta
+
 	protected String filePath;
 	
 	public IReader(String filePath) {
