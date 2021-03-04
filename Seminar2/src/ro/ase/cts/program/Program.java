6d16d37e920d8f52c26c26f321ff489d027d4e24
anaxmaria <63149698+anaxmaria@users.noreply.github.com>
2021-03-04 19:27:22 +0200

    Origin (#1)
    * seminar2 first commit
    
    * adaugare readers, respecta principiile

---
+++ Seminar2/src/ro/ase/cts/program/Program.java
@@ -0,0 +1,31 @@
+package ro.ase.cts.program;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.readers.AngajatiReader;
+import ro.ase.cts.clase.readers.IReader;
+
+public class Program {
+	
+	public static List<Aplicant> citesteAplicanti(IReader readAplicant) throws NumberFormatException, FileNotFoundException{
+		//e functie -> static, altfel era metoda
+		
+		return readAplicant.readAplicants();
+	}
+
+	public static void main(String[] args) {
+		List<Aplicant> listaAplicanti;
+		try {
+			listaAplicanti = citesteAplicanti(new AngajatiReader("angajati.txt"));
+			for(Aplicant aplicant:listaAplicanti)
+				System.out.println(aplicant.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
